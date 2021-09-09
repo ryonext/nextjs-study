ed79e4d231ff57686f75397f67e4e8505ad03d32
Ryoji Shimamoto <shimaryo@amazon.com>
2021-09-09 19:51:55 +0900

    Add first page.

---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+export default function FirstPost({json}) {
+  return <h1>{json.id} {json.joke}</h1>
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://icanhazdadjoke.com/',
+    {
+      headers: {
+        'Accept': 'application/json'
+      }  
+    }
+  )
+  const json = await res.json()
+  console.log(json)
+  return {
+    props: {
+      json
+    }
+  }
+}
